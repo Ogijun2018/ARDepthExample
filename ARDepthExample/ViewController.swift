991f127746d0a38f700f700159841b1e58690780
Jun Ogino <ogijun.mk@hotmail.co.jp>
2023-06-13 15:14:39 +0900

    ✨ Feature: Measure the distance to the object in the center of the screen, and pause the session if raycase result becomes nil

--- ARDepthExample/ViewController.swift
+++ ARDepthExample/ViewController.swift
@@ -9,24 +9,79 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
+    var session: ARSession!
     @IBOutlet var sceneView: ARSCNView!
-    
+    var line: UIView = {
+        let line = UIView()
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.widthAnchor.constraint(equalToConstant: 2).isActive = true
+        line.backgroundColor = .yellow
+        return line
+    }()
+
+    var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .boldSystemFont(ofSize: 40)
+        label.textColor = .green
+        return label
+    }()
+    // 初回起動が完了した場合true
+    var flag = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        session = ARSession()
+        session.delegate = self
+
         // Set the view's delegate
         sceneView.delegate = self
-        
+
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        sceneView.session = session
+
+        sceneView.addSubview(label)
+        sceneView.addSubview(line)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: sceneView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: sceneView.centerYAnchor),
+            line.centerXAnchor.constraint(equalTo: sceneView.centerXAnchor),
+            line.centerYAnchor.constraint(equalTo: sceneView.centerYAnchor),
+            line.heightAnchor.constraint(equalTo: sceneView.heightAnchor)
+        ])
+    }
+
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        let hoge = CGPoint(x: sceneView.frame.midX, y: sceneView.frame.midY)
+        guard let raycast = sceneView.raycastQuery(from: hoge, allowing: .estimatedPlane, alignment: .any),
+              let result = sceneView.session.raycast(raycast).first else {
+            if (flag) {
+                session.pause()
+            }
+            label.text = "Stop!"
+            label.textColor = .red
+            return
+        }
+        if !flag { flag = true }
+        label.textColor = .purple
+
+        let position = SCNVector3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z)
+
+        let transform = frame.camera.transform.columns.3
+        let cameraCoordinates = SCNVector3(x: transform.x, y: transform.y, z: transform.z)
+
+        let distance = cameraCoordinates.distance(to: position)
+        label.text = "Distance: \(String(format: "%.3f", distance)) m"
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,15 +90,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        // sceneDepth(LiDAR)が利用可能かチェック
+        if type(of: configuration).supportsFrameSemantics(.sceneDepth) {
+            configuration.frameSemantics = .sceneDepth
+        }
+
+        print("fps:", configuration.videoFormat.framesPerSecond)
         // Run the view's session
-        sceneView.session.run(configuration)
+        session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the view's session
-        sceneView.session.pause()
+        session.pause()
     }
 
     // MARK: - ARSCNViewDelegate
@@ -72,3 +133,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
 }
+
+extension SCNVector3 {
+     func distance(to vector: SCNVector3) -> Float {
+         return simd_distance(simd_float3(self), simd_float3(vector))
+     }
+ }
